62b7ed5afc826e5192f4705728a9b3b5b66bbb3d
TantoFaz <thiago.playgames15@gmail.com>
2022-12-22 00:03:24 -0300

    starting

--- pages/api/time.js
+++ pages/api/time.js
@@ -1,8 +1,11 @@
-function time(request, response) {
+async function time(request, response) {
+    const apiSecret = 0 | process.env.API_SECRET 
     const dynamicDate = new Date()
+    const subscribers = parseInt(Math.random() * 10000)
 
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        subscribers: subscribers
     })
 }
 
